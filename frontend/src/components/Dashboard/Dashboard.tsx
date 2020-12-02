2ba0edd7ac069d23006be1485e7d3812f7fd7c70
Alberto <algmebe1@gmail.com>
2020-12-02 18:22:54 +0100

    components

---
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+function Dashboard () {
+  return (
+      <Text>
+          Dashboard works!
+      </Text>
+  )
+}
+
+export default Dashboard
