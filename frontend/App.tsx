e627b85df9f6c859ffa44b589415af4421c1d7f2
Alberto <algmebe1@gmail.com>
2020-12-04 12:39:23 +0100

    Tab.Navigator implemented, navigation issues fixed

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,23 +1,94 @@
 import React from 'react'
 import { View } from 'react-native'
 import { Provider as ReduxProvider } from 'react-redux'
+import 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 import HeaderApp from './src/components/Header/Header'
+import Dashboard from './src/components/Dashboard/Dashboard'
+import List from './src//components/List/List'
+import Events from './src/components/Events/Events'
+import Forum from './src/components/Forum/Forum'
+import Marketplace from './src/components/Marketplace/Marketplace'
 import GameDetail from './src/components/GameDetail/GameDetail'
 import configureStore from './src/redux/configureStore'
-import FooterNav from './src/components/FooterNav/FooterNav'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 
 const store = configureStore({ gameReducer: { gameItem: {} } })
+const Tab = createBottomTabNavigator()
 
 function App () {
   return (
       <ReduxProvider store={store}>
           <NavigationContainer>
               <HeaderApp />
-              <View>
-                  <GameDetail />
-              </View>
-              <FooterNav />
+              <Tab.Navigator
+                  screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                      let iconName
+
+                      if (route.name === 'Dashboard') {
+                        iconName = focused
+                          ? 'home'
+                          : 'home'
+                      } else if (route.name === 'List') {
+                        iconName = focused
+                          ? 'list'
+                          : 'list'
+                      } else if (route.name === 'Events') {
+                        iconName = focused
+                          ? 'trophy'
+                          : 'trophy'
+                      } else if (route.name === 'Forum') {
+                        iconName = focused
+                          ? 'comments'
+                          : 'comments'
+                      } else if (route.name === 'Marketplace') {
+                        iconName = focused
+                          ? 'store'
+                          : 'store'
+                      }
+
+                      return (<FontAwesome5
+                          color={color}
+                          name={iconName}
+                          size={size}
+                              />)
+                    },
+                    tabBarButton: [
+                      'GameDetail'
+                    ].includes(route.name)
+                      ? () => {
+                          return null
+                        } : undefined
+                  })}
+              >
+                  <Tab.Screen
+                      component={Dashboard}
+                      name="Dashboard"
+                  />
+                  <Tab.Screen
+                      component={List}
+                      name="List"
+                  />
+                  <Tab.Screen
+                      component={Events}
+                      name="Events"
+                  />
+                  <Tab.Screen
+                      component={Forum}
+                      name="Forum"
+                  />
+                  <Tab.Screen
+                      component={Marketplace}
+                      name="Marketplace"
+                  />
+                  <Tab.Screen
+                      component={GameDetail}
+                      name="GameDetail"
+                  />
+              </Tab.Navigator>
           </NavigationContainer>
       </ReduxProvider>
   )
