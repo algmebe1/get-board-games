62bb80a5294e9f4864b925709b000f8b8bb80130
Alberto <algmebe1@gmail.com>
2020-12-04 12:40:04 +0100

    navigation buttons added to Dashboard

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,11 +1,38 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View, Button } from 'react-native'
 
-function Dashboard () {
+function Dashboard ({ navigation }) {
   return (
-      <Text>
-          Dashboard works!
-      </Text>
+      <View>
+
+          <Text>
+              Dashboard works!
+          </Text>
+
+          <View style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around' }}>
+              <Button
+                  onPress={() => navigation.navigate('Dashboard')}
+                  title="Go to Dashboard"
+              />
+              <Button
+                  onPress={() => navigation.navigate('List')}
+                  title="Go to List"
+              />
+              <Button
+                  onPress={() => navigation.navigate('Events')}
+                  title="Go to Events"
+              />
+              <Button
+                  onPress={() => navigation.navigate('Forum')}
+                  title="Go to Forum"
+              />
+              <Button
+                  onPress={() => navigation.navigate('Marketplace')}
+                  title="Go to Marketplace"
+              />
+          </View>
+
+      </View>
   )
 }
 
