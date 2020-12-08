3a6617bbbfe25966179d7d4643a6512f2e3eec21
Alberto <algmebe1@gmail.com>
2020-12-08 16:32:59 +0100

    user actioTypes added

--- frontend/src/redux/actions/actionTypes.ts
+++ frontend/src/redux/actions/actionTypes.ts
@@ -1,11 +1,15 @@
 interface actionTypesStore {
   LOAD_GAME: string,
   LOAD_GAME_ERROR: string,
+  SEND_USER: string,
+  SEND_USER_ERROR: string
 }
 
 const actionTypes: actionTypesStore = {
   LOAD_GAME: 'LOAD_GAME',
-  LOAD_GAME_ERROR: 'LOAD_GAME_ERROR'
+  LOAD_GAME_ERROR: 'LOAD_GAME_ERROR',
+  SEND_USER: 'SEND_USER',
+  SEND_USER_ERROR: 'SEND_USER_ERROR'
 }
 
 export default actionTypes
