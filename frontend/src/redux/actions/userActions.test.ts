cf4cf0572e4c679f6eb726a586e4c743012c7f3d
Alberto <algmebe1@gmail.com>
2020-12-08 16:35:16 +0100

    userActions and test added

---
+++ frontend/src/redux/actions/userActions.test.ts
@@ -0,0 +1,40 @@
+import axios from 'axios'
+import thunk from 'redux-thunk'
+import configureMockStore from 'redux-mock-store'
+import * as userActions from './userActions'
+import actionTypes from './actionTypes'
+
+const mockStore = configureMockStore([thunk])
+
+jest.mock('axios')
+
+describe('userActions', () => {
+  describe('sendUser function should be called with a promise...', () => {
+    let testData = null
+    let store = null
+
+    beforeEach(() => {
+      testData = { user: 'Skylab mola!' }
+      store = mockStore()
+    })
+
+    afterEach(() => {
+      jest.resetAllMocks()
+    })
+
+    test('resolved and call axios.post function', async () => {
+      const userId = '12345'
+      axios.post = jest.fn().mockResolvedValueOnce(testData)
+      await store.dispatch(userActions.sendUser(userId))
+
+      expect(store.getActions()[0].type).toBe(actionTypes.SEND_USER)
+    })
+    test('resolved and call axios.post function', async () => {
+      const userId = null
+      axios.post = jest.fn().mockRejectedValueOnce(testData)
+      await store.dispatch(userActions.sendUser(userId))
+
+      expect(store.getActions()[0].type).toBe(actionTypes.SEND_USER_ERROR)
+    })
+  })
+})
--- frontend/src/redux/actions/userActions.ts
+++ frontend/src/redux/actions/userActions.ts
@@ -1,14 +1,28 @@
 import axios from 'axios'
 import actionTypes from './actionTypes'
 
-export default function sendUser (userInfo) {
+function sendUserSuccess (userItem: Object) {
+  return {
+    type: actionTypes.SEND_USER,
+    userItem
+  }
+}
+
+function sendUserError (error: any) {
+  return {
+    type: actionTypes.SEND_USER_ERROR,
+    error
+  }
+}
+
+export function sendUser (userInfo) {
   return async (dispatch: Function) => {
     const endpoint = 'http://192.168.0.21:7777/users/'
     try {
       const userItem = await axios.post(endpoint, userInfo)
-      dispatch(userItem)
+      dispatch(sendUserSuccess(userItem))
     } catch (error) {
-      dispatch(error)
+      dispatch(sendUserError(error))
     }
   }
 }
