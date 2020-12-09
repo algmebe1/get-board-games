90717bdc015324860202360fc8f24dc57b4e6ed6
Alberto <algmebe1@gmail.com>
2020-12-09 16:11:37 +0100

    style added in Dashboard

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Text, View, Button } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 function Dashboard () {
   return (
-      <View>
+      <View style={styles.body}>
 
           <Text>
               Dashboard works!
@@ -14,3 +14,10 @@ function Dashboard () {
 }
 
 export default Dashboard
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: 'white',
+    flex: 1
+  }
+})
