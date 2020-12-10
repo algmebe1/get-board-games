4835b354597f7b6b4cc50479ab49896a0cfc3b7b
Alberto <algmebe1@gmail.com>
2020-12-10 13:08:26 +0100

    gameactions test added

--- frontend/src/redux/actions/gameActions.test.ts
+++ frontend/src/redux/actions/gameActions.test.ts
@@ -37,4 +37,39 @@ describe('gameActions', () => {
       expect(store.getActions()[0].type).toBe(actionTypes.LOAD_GAME_ERROR)
     })
   })
+
+  describe('requestAllGames function should be called with a promise...', () => {
+    let testData = null
+    let store = null
+
+    beforeEach(() => {
+      testData = { data: [{ name: 'Skylab mola!' }] }
+      store = mockStore()
+    })
+
+    afterEach(() => {
+      jest.resetAllMocks()
+    })
+
+    test('resolved an return an object with property data', async () => {
+      axios.get = jest.fn().mockResolvedValueOnce(testData)
+      await store.dispatch(gameActions.requestAllGames())
+      expect(store.getActions()[0].type).toBe(actionTypes.LOAD_ALL_GAMES)
+    })
+
+    test('resolved an return an object with property data', async () => {
+      axios.get = jest.fn().mockRejectedValueOnce(testData)
+      await store.dispatch(gameActions.requestAllGames())
+      expect(store.getActions()[0].type).toBe(actionTypes.LOAD_ALL_GAMES_ERROR)
+    })
+  })
+
+  describe('requestAllGamesSuccess', () => {
+    test('should order ranking from highest to lowest', () => {
+      const gameCollection = [{ rank: 2 }, { rank: 1 }]
+
+      const result = gameActions.requestAllGamesSuccess(gameCollection)
+      expect(result.gameCollection).toEqual([{ rank: 1 }, { rank: 2 }])
+    })
+  })
 })
