95b2fd4ebf50f005b5e51d5144ab4493aeaabb9e
Alberto <algmebe1@gmail.com>
2020-12-11 16:06:09 +0100

    Header navigation to profile added

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import { connect } from 'react-redux'
+import { props } from '../../interfaces/interfaces'
 import firebase from 'firebase'
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+import * as RootNavigation from '../Application/RootNavigation.js'
+import { sendUser } from '../../redux/actions/userActions'
 
-function HeaderApp () {
+function HeaderApp ({ userItem, dispatch }: props) {
   return (
       <View style={styles.header}>
           <View style={styles.headerItems}>
@@ -12,17 +16,39 @@ function HeaderApp () {
                   source={{ uri: 'https://trello-attachments.s3.amazonaws.com/5fbbdffec6c8c916bd924758/658x652/89ee36969caa01ee422982a49f59fc06/GBG-logo-white.png' }}
                   style={styles.logo}
               />
-              <TouchableOpacity onPress={() => firebase.auth().signOut()}>
-                  <FontAwesome5
-                      name="power-off"
-                      size={20}
-                      style={{ color: 'white' }}
-                  />
-              </TouchableOpacity>
+              {userItem?.id
+                ? (<View style={styles.userOptions}>
+                    <TouchableOpacity
+                        onPress={() => RootNavigation.navigate('Profile', { userItem })}
+                    >
+                        <Image
+                            source={{ uri: userItem?.photoUrl }}
+                            style={{ width: 40, height: 40, borderRadius: 20, marginRight: 10 }}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => {
+                      firebase.auth().signOut()
+                      dispatch(sendUser(null))
+                      RootNavigation.navigate('LoginWithGoogle')
+                    }}
+                    >
+                        <FontAwesome5
+                            name="power-off"
+                            size={25}
+                            style={{ color: 'white' }}
+                        />
+                    </TouchableOpacity>
+                   </View>) : null}
           </View>
 
       </View>
   )
+}
+
+function mapStateToProps (state: any) {
+  return {
+    userItem: state?.userReducer?.userObject
+  }
 }
 
 const styles = StyleSheet.create({
@@ -55,8 +81,14 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     width: 60,
     height: 60
+  },
+
+  userOptions: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
   }
 
 })
 
-export default HeaderApp
+export default connect(mapStateToProps)(HeaderApp)
