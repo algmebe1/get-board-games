0b8789e33881c56dedc3713a86b93aff2d21de66
Alberto <algmebe1@gmail.com>
2020-12-11 16:05:10 +0100

    LoginWithGoogle component created

---
+++ frontend/src/components/LoginWithGoogle/LoginWithGoogle.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native'
+import { connect } from 'react-redux'
+import { loginGoogle, sendUser } from '../../redux/actions/userActions'
+
+function LoginWithGoogle ({ dispatch, user, navigation }: props) {
+  function handleLoggingClick () {
+    dispatch(loginGoogle())
+  }
+  useEffect(() => {
+    if (user.id) {
+      dispatch(sendUser({ id: user?.id, name: user?.name, photoUrl: user?.photoUrl }))
+      navigation.navigate('Application', { screen: 'Dashboard' })
+    } else {
+      navigation.navigate('LoginWithGoogle')
+    }
+  }, [user])
+
+  return (
+      <View style={styles.container}>
+          <Text>
+              Please Sign in to get into the app!
+          </Text>
+          <View style={{ width: 200, marginTop: 20 }}>
+              <Button
+                  onPress={handleLoggingClick}
+                  title="Sign In With Google"
+              />
+          </View>
+      </View>
+  )
+}
+
+function mapStateToProps ({ userReducer }: any) {
+  return {
+    user: userReducer.user
+  }
+}
+
+export default connect(mapStateToProps)(LoginWithGoogle)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column'
+  }
+})
