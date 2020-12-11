79425f00c27dd2d5db1b86b17e13c64c01ef16cc
Alberto <algmebe1@gmail.com>
2020-12-11 15:51:31 +0100

    Login actions added

--- frontend/src/redux/actions/userActions.ts
+++ frontend/src/redux/actions/userActions.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import actionTypes from './actionTypes'
+import signInWithGoogle from '../../../firebase.js'
 
 function sendUserSuccess (userItem: Object) {
   return {
@@ -15,12 +16,38 @@ function sendUserError (error: any) {
   }
 }
 
+function loggingGoogleSuccess (user: Object) {
+  return {
+    type: actionTypes.LOGIN_USER_GOOGLE,
+    user
+  }
+}
+
+function loggingGoogleError (error: any) {
+  return {
+    type: actionTypes.LOGIN_USER_GOOGLE_ERROR,
+    error
+  }
+}
+
+export function loginGoogle () {
+  return async (dispatch: Function) => {
+    try {
+      const { user } = await signInWithGoogle()
+      console.log(user)
+      dispatch(loggingGoogleSuccess(user))
+    } catch (error) {
+      dispatch(loggingGoogleError(error))
+    }
+  }
+}
+
 export function sendUser (userInfo) {
   return async (dispatch: Function) => {
     const endpoint = 'http://192.168.0.21:7777/users/'
     try {
       const userItem = await axios.post(endpoint, userInfo)
-      dispatch(sendUserSuccess(userItem))
+      dispatch(sendUserSuccess(userItem.data))
     } catch (error) {
       dispatch(sendUserError(error))
     }
