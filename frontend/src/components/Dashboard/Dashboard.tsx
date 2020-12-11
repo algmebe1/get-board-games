dd0ea4e0efb85235a558024337360f3f48ceeddb
Alberto <algmebe1@gmail.com>
2020-12-11 16:15:14 +0100

    redux components added to dashboard

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,19 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
-
-function Dashboard () {
-  return (
-      <View style={styles.body}>
-
-          <Text>
-              Dashboard works!
-          </Text>
-
-      </View>
-  )
-}
-
-export default Dashboard
+import { connect } from 'react-redux'
+import { props } from '../../interfaces/interfaces'
 
 const styles = StyleSheet.create({
   body: {
@@ -21,3 +9,29 @@ const styles = StyleSheet.create({
     flex: 1
   }
 })
+
+function Dashboard ({ userItem }: props) {
+  useEffect(() => {
+    console.log('Useeffect:', userItem)
+  }, [userItem])
+  return (
+      <View style={styles.body}>
+
+          <Text>
+              {userItem?.name}
+          </Text>
+
+      </View>
+  )
+}
+
+function mapStateToProps (state: any) {
+  console.log('\n\nMAPSTATETOPROPS: ')
+  console.log(state)
+
+  return {
+    userItem: state?.userReducer?.userObject
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard)
