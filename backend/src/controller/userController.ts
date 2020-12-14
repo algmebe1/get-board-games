4d985647011c2470688dd4df2414120a7610df34
Alberto <algmebe1@gmail.com>
2020-12-14 13:05:41 +0100

    get and patch functions added to userController

--- backend/src/controller/userController.ts
+++ backend/src/controller/userController.ts
@@ -2,6 +2,26 @@ import { Request, Response } from 'express'
 export {}
 
 function userController (User: any) {
+  function getMethod (req: Request, res: Response) {
+    const { userId } = req.params
+    const query = { id: userId }
+
+    User.findOne(query, (error: any, currentUser: any) => {
+      error ? res.send(error) : res.json(currentUser)
+    })
+  }
+
+  function patchMethod (req: Request, res: Response) {
+    const query = req.params.userId
+    const body = req.body
+
+    User.findByIdAndUpdate(query, body, { new: true }).populate('favourites').populate('events').exec(
+      (error, user) => {
+        error ? res.send(error) : res.json(user)
+      }
+    )
+  }
+
   function postMethod (req: Request, res: Response) {
     const query = { id: req.body.id }
 
@@ -10,7 +30,7 @@ function userController (User: any) {
     })
   }
 
-  return { postMethod }
+  return { getMethod, patchMethod, postMethod }
 }
 
 module.exports = userController
