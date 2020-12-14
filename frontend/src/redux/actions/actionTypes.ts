2492853922288b0ec6d73c55b85a280ac7091258
Alberto <algmebe1@gmail.com>
2020-12-14 12:59:23 +0100

    actionTypes updated

--- frontend/src/redux/actions/actionTypes.ts
+++ frontend/src/redux/actions/actionTypes.ts
@@ -1,23 +1,21 @@
 interface actionTypesStore {
   LOAD_GAME: string,
-  LOAD_GAME_ERROR: string,
   LOAD_ALL_GAMES: string,
-  LOAD_ALL_GAMES_ERROR: string,
+  LOAD_USER: string,
   SEND_USER: string,
-  SEND_USER_ERROR: string,
   LOGIN_USER_GOOGLE: string,
-  LOGIN_USER_GOOGLE_ERROR: string
+  LOGOUT_USER: string,
+  LOAD_ERROR: string,
 }
 
 const actionTypes: actionTypesStore = {
   LOAD_GAME: 'LOAD_GAME',
-  LOAD_GAME_ERROR: 'LOAD_GAME_ERROR',
   LOAD_ALL_GAMES: 'LOAD_ALL_GAMES',
-  LOAD_ALL_GAMES_ERROR: 'LOAD_ALL_GAMES_ERROR',
+  LOAD_USER: 'LOAD_USER',
   SEND_USER: 'SEND_USER',
-  SEND_USER_ERROR: 'SEND_USER_ERROR',
   LOGIN_USER_GOOGLE: 'LOGIN_USER_GOOGLE',
-  LOGIN_USER_GOOGLE_ERROR: 'LOGIN_USER_GOOGLE_ERROR'
+  LOGOUT_USER: 'LOGOUT_USER',
+  LOAD_ERROR: 'LOAD_ERROR'
 }
 
 export default actionTypes
