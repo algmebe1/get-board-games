ce7f8cca9f8a69a327d572ff4a47217441905406
Alberto <algmebe1@gmail.com>
2020-12-15 19:17:07 +0100

    Dashboard file deleted

--- frontend/src/components/Dashboard/Dashboard.test.ts
+++
@@ -1,9 +0,0 @@
-import Dashboard from './Dashboard'
-
-describe('Dashboard', () => {
-  test('should render as expected', () => {
-    const dashboardFunction = Dashboard()
-
-    expect(dashboardFunction).toBeDefined()
-  })
-})
