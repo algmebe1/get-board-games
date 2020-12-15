95f857ffae795237edaf5ee3a91b4b6006e0c111
Alberto <algmebe1@gmail.com>
2020-12-15 12:19:36 +0100

    typeErrors refactored and addGame and addGameToFav

--- frontend/src/redux/actions/gameActions.ts
+++ frontend/src/redux/actions/gameActions.ts
@@ -20,14 +20,14 @@ export function requestAllGamesSuccess (gameCollection: Array) {
 
 function loadError (error: any) {
   return {
-    type: actionTypes.LOAD_GAME_ERROR,
+    type: actionTypes.LOAD_ERROR,
     error
   }
 }
 
 export function requestGame (gameId: string) {
   return async (dispatch: Function) => {
-    const endpoint = `http://192.168.1.36:7777/games/${gameId}`
+    const endpoint = `http://192.168.0.21:7777/games/${gameId}`
     try {
       const gameItem = await axios.get(endpoint)
       dispatch(requestGameSuccess(gameItem.data))
@@ -46,6 +46,28 @@ export function requestAllGames () {
       const gameCollection = await axios.get(endpoint)
       debugger
       dispatch(requestAllGamesSuccess(gameCollection.data))
+    } catch (error) {
+      dispatch(loadError(error))
+    }
+  }
+}
+
+export function addGameToFav (userObj, gameItem) {
+  const newObj = { ...userObj }
+
+  if (newObj.favourites.length < 4) {
+    newObj.favourites.push(gameItem)
+  }
+  return newObj
+}
+
+export function addGame (userObject: Object, gameItem: Object) {
+  return async (dispatch: Function) => {
+    const newObj = addGameToFav(userObject, gameItem)
+    const endpoint = `http://192.168.0.21:7777/users/favourites/${newObj.id}`
+    try {
+      const objectUpdated = await axios.patch(endpoint, { favourites: newObj.favourites })
+      console.log(objectUpdated)
     } catch (error) {
       dispatch(loadError(error))
     }
