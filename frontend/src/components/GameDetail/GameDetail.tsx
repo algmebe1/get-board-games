8784067417574802320532d2f868864bfcf235c8
Alberto <algmebe1@gmail.com>
2020-12-16 09:18:26 +0100

    GameDetail file refactored

--- frontend/src/components/GameDetail/GameDetail.tsx
+++ frontend/src/components/GameDetail/GameDetail.tsx
@@ -6,104 +6,9 @@ import { Button } from 'react-native-elements'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import BackButton from '../Header/BackButton/BackButton'
 import { connect } from 'react-redux'
-import { props } from '../../interfaces/interfaces'
+import { propsInterface } from '../../interfaces/interfaces'
 import HomeButton from '../Header/HomeButton/HomeButton'
 import { addGame } from '../../redux/actions/gameActions'
-
-function GameDetail ({ route: { params: { gameItem } }, userObject, dispatch }: props) {
-  const detailScrollRef = useRef(null)
-
-  const isFocused = useIsFocused()
-  useEffect(() => {
-    detailScrollRef.current.scrollTo({ x: 0, y: 0, animated: false })
-  }, [isFocused])
-
-  return (
-      <ScrollView
-          contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}
-          ref={detailScrollRef}
-      >
-          <View />
-          <View style={styles.imageContainer}>
-              <Image
-                  resizeMode='contain'
-                  source={{ uri: gameItem?.images.original }}
-                  style={{ width: 300, height: 300 }}
-              />
-          </View>
-          <View style={styles.navButtons}>
-              <BackButton />
-              <HomeButton />
-          </View>
-          <View style={styles.info}>
-              <View style={styles.titleContainer}>
-                  <Text style={styles.title}>
-                      {gameItem?.name.toUpperCase()}
-                  </Text>
-
-              </View>
-              <View style={styles.titleBar} />
-              <View style={styles.descriptionContainer}>
-                  <Text style={styles.description}>
-                      {gameItem?.description_preview}
-                  </Text>
-                  <View style={styles.gameinfo}>
-                      <View style={styles.players}>
-                          <FontAwesome5
-                              name='users'
-                              style={{ fontSize: 20 }}
-                          />
-                          <Text>
-                              {' '}
-                              {' '}
-                              {' '}
-                              {gameItem?.min_players !== gameItem?.max_players ? `${gameItem?.min_players} - ${gameItem?.max_players} players` : `${gameItem?.min_players} players`}
-                          </Text>
-                      </View>
-                      <View style={styles.time}>
-                          <FontAwesome5
-                              name='clock'
-                              style={{ fontSize: 20 }}
-                          />
-                          <Text>
-                              {' '}
-                              {' '}
-                              {' '}
-                              {gameItem?.min_playtime !== gameItem?.max_playtime ? `${gameItem?.min_playtime} - ${gameItem?.max_playtime} min` : `${gameItem.min_players} min`}
-                          </Text>
-                      </View>
-                  </View>
-                  <View style={styles.buttonContainer}>
-                      <Button
-                          icon={
-                              <Icon
-                                  color="white"
-                                  name="plus"
-                                  size={15}
-                              />
-  }
-                          onPress={() => {
-                            dispatch(addGame(userObject, gameItem))
-                          }}
-                          style={styles.addButton}
-
-                          title="  Add to favourites"
-                      />
-                  </View>
-              </View>
-          </View>
-          <View style={{ width: '100%', height: 100 }} />
-      </ScrollView>
-  )
-}
-
-function mapStateToProps ({ gameReducer, userReducer }: any) {
-  debugger
-  return {
-    gameItem: gameReducer.gameObject,
-    userObject: userReducer.userObject
-  }
-}
 
 const styles = StyleSheet.create({
   navButtons: {
@@ -204,5 +109,100 @@ const styles = StyleSheet.create({
   }
 
 })
+
+function GameDetail ({ route: { params: { gameItem } }, userObject, dispatch }: propsInterface) {
+  const detailScrollRef = useRef(null)
+
+  const isFocused = useIsFocused()
+  useEffect(() => {
+    detailScrollRef.current.scrollTo({ x: 0, y: 0, animated: false })
+  }, [isFocused])
+
+  return (
+      <ScrollView
+          contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}
+          ref={detailScrollRef}
+      >
+          <View />
+          <View style={styles.imageContainer}>
+              <Image
+                  resizeMode='contain'
+                  source={{ uri: gameItem?.images.original }}
+                  style={{ width: 300, height: 300 }}
+              />
+          </View>
+          <View style={styles.navButtons}>
+              <BackButton />
+              <HomeButton />
+          </View>
+          <View style={styles.info}>
+              <View style={styles.titleContainer}>
+                  <Text style={styles.title}>
+                      {gameItem?.name.toUpperCase()}
+                  </Text>
+
+              </View>
+              <View style={styles.titleBar} />
+              <View style={styles.descriptionContainer}>
+                  <Text style={styles.description}>
+                      {gameItem?.description_preview}
+                  </Text>
+                  <View style={styles.gameinfo}>
+                      <View style={styles.players}>
+                          <FontAwesome5
+                              name='users'
+                              style={{ fontSize: 20 }}
+                          />
+                          <Text>
+                              {' '}
+                              {' '}
+                              {' '}
+                              {gameItem?.min_players !== gameItem?.max_players ? `${gameItem?.min_players} - ${gameItem?.max_players} players` : `${gameItem?.min_players} players`}
+                          </Text>
+                      </View>
+                      <View style={styles.time}>
+                          <FontAwesome5
+                              name='clock'
+                              style={{ fontSize: 20 }}
+                          />
+                          <Text>
+                              {' '}
+                              {' '}
+                              {' '}
+                              {gameItem?.min_playtime !== gameItem?.max_playtime ? `${gameItem?.min_playtime} - ${gameItem?.max_playtime} min` : `${gameItem.min_players} min`}
+                          </Text>
+                      </View>
+                  </View>
+                  <View style={styles.buttonContainer}>
+                      <Button
+                          icon={
+                              <Icon
+                                  color="white"
+                                  name="plus"
+                                  size={15}
+                              />
+  }
+                          onPress={() => {
+                            dispatch(addGame(userObject, gameItem))
+                          }}
+                          style={styles.addButton}
+
+                          // eslint-disable-next-line react/jsx-props-no-multi-spaces
+                          title="  Add to favourites"
+                      />
+                  </View>
+              </View>
+          </View>
+          <View style={{ width: '100%', height: 100 }} />
+      </ScrollView>
+  )
+}
+
+function mapStateToProps ({ gameReducer, userReducer }: any) {
+  return {
+    gameItem: gameReducer.gameObject,
+    userObject: userReducer.userObject
+  }
+}
 
 export default connect(mapStateToProps)(GameDetail)
