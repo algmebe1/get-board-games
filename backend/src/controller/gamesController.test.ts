3df09f9c3e5820427a082b7bd26542a6b020503c
Alberto <algmebe1@gmail.com>
2020-12-29 15:24:21 +0100

    gamesController patchmethod created and 100% tested

--- backend/src/controller/gamesController.test.ts
+++ backend/src/controller/gamesController.test.ts
@@ -28,4 +28,41 @@ describe('gamesController', () => {
       expect(res.send).toHaveBeenCalled()
     })
   })
+
+  describe('patchMethod', () => {
+    test('should call res.json without error', () => {
+      const req = {
+        params: { gameId: '12345' },
+        body: {
+          id: '12345'
+        }
+      }
+      const res = {
+        json: jest.fn()
+      }
+
+      Game.findByIdAndUpdate = jest.fn().mockImplementationOnce((query, body, options, callback) => callback(false, true))
+
+      gamesController.patchMethod(req, res)
+
+      expect(res.json).toHaveBeenCalled()
+    })
+    test('should call res.send due to an error', () => {
+      const req = {
+        params: { gameId: '12345' },
+        body: {
+          id: '12345'
+        }
+      }
+      const res = {
+        send: jest.fn()
+      }
+
+      Game.findByIdAndUpdate = jest.fn().mockImplementationOnce((query, body, options, callback) => callback(true, true))
+
+      gamesController.patchMethod(req, res)
+
+      expect(res.send).toHaveBeenCalled()
+    })
+  })
 })
