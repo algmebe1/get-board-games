0a2df5b2027f58e45a61b0b5bcadf836d4041770
algmebe1 <algmebe1@gmail.com>
2021-01-31 16:59:46 +0100

    minor rendering error fixed

--- frontend/src/components/GameDetail/GameDetail.tsx
+++ frontend/src/components/GameDetail/GameDetail.tsx
@@ -133,7 +133,7 @@ function GameDetail ({ route: { params: { gameItem } }, userObject, dispatch }: 
   const isFocused = useIsFocused()
   const [loaded, setLoading] = useState(false)
 
-  const checkFavourites = userObject?.favourites.some(i => i.name.includes(gameItem.name))
+  const checkFavourites = userObject?.favourites.some(i => i?.name.includes(gameItem.name))
   useEffect(() => {
     detailScrollRef.current.scrollTo({ x: 0, y: 0, animated: false })
   }, [isFocused])
@@ -155,97 +155,97 @@ function GameDetail ({ route: { params: { gameItem } }, userObject, dispatch }: 
   }
 
   return (
-      <ScrollView
-          contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}
-          ref={detailScrollRef}
-      >
-          <View />
-          <View style={styles.imageContainer}>
-            {loaded
-              ? <Image
-                      resizeMode='contain'
-                      source={{ uri: gameItem?.images.original, cache: 'force-cache'}}
-                      style={{ width: 300, height: 300 }}
-                /> : <ActivityIndicator />}
-          </View>
-          <View style={styles.navButtons}>
-              <BackButton />
-              <HomeButton />
-          </View>
-          <View style={styles.info}>
-              <View style={styles.titleContainer}>
-                  <Text style={styles.title}>
-                      {gameItem?.name.toUpperCase()}
-                  </Text>
+    <ScrollView
+        contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}
+        ref={detailScrollRef}
+    >
+      <View />
+      <View style={styles.imageContainer}>
+        {loaded
+          ? <Image
+                  resizeMode='contain'
+                  source={{ uri: gameItem?.images.original, cache: 'force-cache' }}
+                  style={{ width: 300, height: 300 }}
+            /> : <ActivityIndicator />}
+      </View>
+      <View style={styles.navButtons}>
+        <BackButton />
+        <HomeButton />
+      </View>
+      <View style={styles.info}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>
+            {gameItem?.name.toUpperCase()}
+          </Text>
 
+        </View>
+        <View style={styles.titleBar} />
+        <View style={styles.descriptionContainer}>
+          <Text style={styles.description}>
+            {gameItem?.description_preview}
+          </Text>
+          <View style={styles.gameinfo}>
+            <View style={styles.players}>
+              <FontAwesome5
+                  name='users'
+                  style={{ fontSize: 20 }}
+              />
+              <Text>
+                {' '}
+                {' '}
+                {' '}
+                {gameItem?.min_players !== gameItem?.max_players ? `${gameItem?.min_players} - ${gameItem?.max_players} players` : `${gameItem?.min_players} players`}
+              </Text>
+            </View>
+            <View style={styles.time}>
+              <FontAwesome5
+                  name='clock'
+                  style={{ fontSize: 20 }}
+              />
+              <Text>
+                {' '}
+                {' '}
+                {' '}
+                {gameItem?.min_playtime !== gameItem?.max_playtime ? `${gameItem?.min_playtime} - ${gameItem?.max_playtime} min` : `${gameItem.min_players} min`}
+              </Text>
+            </View>
+          </View>
+          <View style={styles.buttonContainer}>
+            {checkFavourites ? (
+              <View style={styles.gameAddedContainer}>
+                <FontAwesome5
+                    name='check'
+                    style={styles.checkIcon}
+                />
+                <Text style={styles.gameAddedText}>
+
+                  This game is already added
+                </Text>
               </View>
-              <View style={styles.titleBar} />
-              <View style={styles.descriptionContainer}>
-                  <Text style={styles.description}>
-                      {gameItem?.description_preview}
-                  </Text>
-                  <View style={styles.gameinfo}>
-                      <View style={styles.players}>
-                          <FontAwesome5
-                              name='users'
-                              style={{ fontSize: 20 }}
-                          />
-                          <Text>
-                              {' '}
-                              {' '}
-                              {' '}
-                              {gameItem?.min_players !== gameItem?.max_players ? `${gameItem?.min_players} - ${gameItem?.max_players} players` : `${gameItem?.min_players} players`}
-                          </Text>
-                      </View>
-                      <View style={styles.time}>
-                          <FontAwesome5
-                              name='clock'
-                              style={{ fontSize: 20 }}
-                          />
-                          <Text>
-                              {' '}
-                              {' '}
-                              {' '}
-                              {gameItem?.min_playtime !== gameItem?.max_playtime ? `${gameItem?.min_playtime} - ${gameItem?.max_playtime} min` : `${gameItem.min_players} min`}
-                          </Text>
-                      </View>
-                  </View>
-                  <View style={styles.buttonContainer}>
-                    {checkFavourites ? (
-                      <View style={styles.gameAddedContainer}>
-                        <FontAwesome5
-                            name='check'
-                            style={styles.checkIcon}
-                        />
-                      <Text style={styles.gameAddedText}>
+            )
+              : (<Button
+                      icon={
+                  <Icon
+                      color="white"
+                      name="plus"
+                      size={15}
+                  />
+                }
+                      onPress={() => {
+                        dispatch(updateGame(gameItem))
+                        dispatch(addGame(userObject, gameItem))
+                        GameAdded()
+                      }}
+                      style={styles.addButton}
 
-This game is already added
-                      </Text>
-                      </View>
-                    )
-                      : (<Button
-                              icon={
-                              <Icon
-                                  color="white"
-                                  name="plus"
-                                  size={15}
-                              />
-  }
-                              onPress={() => {
-                                dispatch(updateGame(gameItem))
-                                dispatch(addGame(userObject, gameItem))
-                                GameAdded()
-                              }}
-                              style={styles.addButton}
-
-                          // eslint-disable-next-line react/jsx-props-no-multi-spaces
-                              title="  Add to favourites"
-                         />)}
-                  </View>
-              </View>
+                // eslint-disable-next-line react/jsx-props-no-multi-spaces
+                      title="  Add to favourites"
+                 />)}
           </View>
-          <View style={{ width: '100%', height: 100 }} />
-      </ScrollView>
+        </View>
+      </View>
+      <View style={{ width: '100%', height: 100 }} />
+    </ScrollView>
   )
 }
 
