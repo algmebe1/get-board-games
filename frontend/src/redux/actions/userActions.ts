770f5f9353fe26c556e5121edead8c52005a049d
algmebe1 <algmebe1@gmail.com>
2021-04-17 16:25:00 +0200

    types fixed in userActions.ts file

--- frontend/src/redux/actions/userActions.ts
+++ frontend/src/redux/actions/userActions.ts
@@ -1,30 +1,31 @@
 import axios from 'axios'
 import actionTypes from './actionTypes'
 import signInWithGoogle from '../../../firebase.js'
-import { gameItemInterface, userObjectInterface } from '../../interfaces/interfaces'
+import { gameItemInterface, userObjectInterface, user } from '../../interfaces/interfaces'
+import { Dispatch } from 'redux'
 
-function loginGoogleSuccess (user: Object) {
+function loginGoogleSuccess (user: user) {
   return {
     type: actionTypes.LOGIN_USER_GOOGLE,
     user
   }
 }
 
-export function logoutUser (userObject: Object) {
+export function logoutUser (userObject: userObjectInterface) {
   return {
     type: actionTypes.LOGOUT_USER,
     userObject
   }
 }
 
-function loadUserSuccess (userItem: Object) {
+function loadUserSuccess (userItem: userObjectInterface) {
   return {
     type: actionTypes.LOAD_USER,
     userItem
   }
 }
 
-function sendUserSuccess (userItem: Object) {
+function sendUserSuccess (userItem: userObjectInterface) {
   return {
     type: actionTypes.SEND_USER,
     userItem
@@ -38,7 +39,7 @@ function loadError (error: any) {
   }
 }
 
-function updateFavourites (favouritesArray: Object[]) {
+function updateFavourites (favouritesArray: userObjectInterface) {
   return {
     type: actionTypes.LOAD_FAVOURITES,
     favouritesArray
@@ -46,7 +47,7 @@ function updateFavourites (favouritesArray: Object[]) {
 }
 
 export function loginGoogle () {
-  return async (dispatch: Function) => {
+  return async (dispatch: Dispatch<any>) => {
     try {
       const { user } = await signInWithGoogle()
       dispatch(loginGoogleSuccess(user))
@@ -56,9 +57,9 @@ export function loginGoogle () {
   }
 }
 
-export function loadUser (userId: Object) {
-  return async (dispatch: Function) => {
-    const endpoint = `http://192.168.1.100:7777/users/${userId}`
+export function loadUser (userId: string) {
+  return async (dispatch: Dispatch<any>) => {
+    const endpoint = `http://192.168.1.51:7777/users/${userId}`
     try {
       const userItem = await axios.get(endpoint)
       dispatch(loadUserSuccess(userItem.data))
@@ -68,9 +69,9 @@ export function loadUser (userId: Object) {
   }
 }
 
-export function sendUser (userInfo: Object) {
-  return async (dispatch: Function) => {
-    const endpoint = 'http://192.168.1.100:7777/users/'
+export function sendUser (userInfo: {id: string, name: string, photoUrl: string}) {
+  return async (dispatch: Dispatch<any>) => {
+    const endpoint = 'http://192.168.1.51:7777/users/'
     try {
       const userItem = await axios.post(endpoint, userInfo)
       dispatch(sendUserSuccess(userItem.data))
@@ -80,9 +81,9 @@ export function sendUser (userInfo: Object) {
   }
 }
 
-export function saveUserChanges (userId: Object, userDetails: Object) {
-  return async (dispatch: Function) => {
-    const endpoint = `http://192.168.1.100:7777/users/${userId}`
+export function saveUserChanges (userId: string, userDetails: {username: string, location: string, bio: string }) {
+  return async (dispatch: Dispatch<any>) => {
+    const endpoint = `http://192.168.1.51:7777/users/${userId}`
     try {
       await axios.patch(endpoint, userDetails)
     } catch (error) {
@@ -100,9 +101,9 @@ export function deleteGameFromFav (userObj: userObjectInterface, gameItem: gameI
 }
 
 export function deleteGame (userObject: userObjectInterface, gameItem: gameItemInterface) {
-  return async (dispatch: Function) => {
+  return async (dispatch: Dispatch<any>) => {
     const newObj = deleteGameFromFav(userObject, gameItem)
-    const endpoint = `http://192.168.1.100:7777/users/deletefromfavourites/${newObj._id}`
+    const endpoint = `http://192.168.1.51:7777/users/deletefromfavourites/${newObj._id}`
     try {
       await axios.patch(endpoint, { favourites: newObj.favourites })
       dispatch(updateFavourites(newObj))
